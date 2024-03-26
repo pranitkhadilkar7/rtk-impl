d5165ad442cc75caa6060776ecbbb29b82de257c
Pranit Khadilkar <pranit@emtech.com>
2024-03-26 12:20:39 +0530

    added heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 function App() {
-  return <h1>RTK Query Implementation with React</h1>
+  return (
+    <div className="tw-h-full tw-flex tw-flex-col tw-p-6">
+      <div className="tw-flex tw-flex-col tw-flex-wrap tw-content-center">
+        <h1 className="tw-text-xl tw-font-semibold">
+          RTK Query Implementation with React
+        </h1>
+      </div>
+    </div>
+  )
 }
 
 export default App
